6f4c3d5aedd403c7a809d8ffea3edf149736a19f
trentduffy <ct_trentduffy@me.com>
2016-01-24 04:42:49 -0500

    not working fixing notifacions

---
+++ app/src/main/java/com/example/kristychen/ufree3/Receiver.java
@@ -0,0 +1,27 @@
+package com.example.kristychen.ufree3;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParsePushBroadcastReceiver;
+
+import android.content.*;
+import android.util.Log;
+
+/**
+ * Created by trentolol on 1/24/16.
+ */
+public class Receiver extends ParsePushBroadcastReceiver {
+
+    @Override
+    public void onPushOpen(Context context, Intent intent) {
+        Log.e("Push", "Clicked");
+        Intent i = new Intent(context, yesnoActivity.class);
+        i.putExtras(intent.getExtras());
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
